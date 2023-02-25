3b61dabdc0d5c2760013c08cd940c1f547fb3da0
Klaus Aigner <kaigner@shemhazai.de>
2023-02-25 17:37:03 +0100

    Codecleanup & add Webserver Lib

--- src/main.cpp
+++ src/main.cpp
@@ -9,25 +9,15 @@ int pos = 0;    // variable to store the servo position
 int servoPin = 16;
 
 void setup() {
-//  // Allow allocation of all timers
-//  ESP32PWM::allocateTimer(0);
-//  ESP32PWM::allocateTimer(1);
-//  ESP32PWM::allocateTimer(2);
-//  ESP32PWM::allocateTimer(3);
-//  myservo.setPeriodHertz(50);    // standard 50 hz servo
-
-
   myservo.attach(servoPin);
   myservo.write(90);
 }
 void loop() {
   myservo.write(45); // rotate the motor counterclockwise
-  delay(10000); // keep rotating for 5 seconds (5000 milliseconds)
+  delay(10000); // 10 secs
 
   myservo.write(90); // stop the motor
-  delay(20000); // stay stopped
+  delay(20000); // stay stopped 20 secs
 
   //myservo.write(135); // rotate the motor clockwise
-
-  //delay(5000); // keep rotating :D
 }
