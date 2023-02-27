fd1b6c9962115530637d626faac35f3e772c5a51
Klaus Aigner <kaigner@shemhazai.de>
2023-02-27 20:01:23 +0100

    add: Base tests und test Code ;)  Servo Speed, Website tinghs

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,149 @@
 #include <Arduino.h>
 #include <ESP32Servo.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
 #include "config.h"
 
 
 Servo myservo;
-
-int pos = 0;    // variable to store the servo position
 int servoPin = 16;
 
+AsyncWebServer server(80);
+const char* PARAM_INPUT_1 = "output";
+const char* PARAM_INPUT_2 = "state";
+
+
+
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html>
+<head>
+  <title>Cat Feeder</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" href="data:,">
+  <style>
+    html {font-family: Arial; display: inline-block; text-align: center;}
+    h2 {font-size: 3.0rem;}
+    p {font-size: 3.0rem;}
+    body {max-width: 600px; margin:0px auto; padding-bottom: 25px;}
+    .switch {position: relative; display: inline-block; width: 120px; height: 68px} 
+    .switch input {display: none}
+    .slider {position: absolute; top: 0; left: 0; right: 0; bottom: 0; background-color: #ccc; border-radius: 6px}
+    .slider:before {position: absolute; content: ""; height: 52px; width: 52px; left: 8px; bottom: 8px; background-color: #fff; -webkit-transition: .4s; transition: .4s; border-radius: 3px}
+    input:checked+.slider {background-color: #b30000}
+    input:checked+.slider:before {-webkit-transform: translateX(52px); -ms-transform: translateX(52px); transform: translateX(52px)}
+  </style>
+</head>
+<body>
+  <h2>Cat Feeder</h2>
+  %BUTTONPLACEHOLDER%
+<script>function toggleCheckbox(element) {
+  var xhr = new XMLHttpRequest();
+  if(element.checked){ xhr.open("GET", "/update?output="+element.id+"&state=1", true); }
+  else { xhr.open("GET", "/update?output="+element.id+"&state=0", true); }
+  xhr.send();
+}
+</script>
+</body>
+</html>
+)rawliteral";
+
+
+String outputState(int output){
+  if(digitalRead(output)){
+    return "checked";
+  }
+  else {
+    return "";
+  }
+}
+
+String processor(const String& var){
+  Serial.println(var);
+  if(var == "BUTTONPLACEHOLDER"){
+    String buttons = "";
+    buttons += "<h4>Feed Cat 1 meal - GPIO 2</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"2\" " + outputState(2) + "><span class=\"slider\"></span></label>";
+    return buttons;
+  }
+  return String();
+}
+
+
+
 void setup() {
+  Serial.begin(115200);
   myservo.attach(servoPin);
   myservo.write(90);
+
+  pinMode(2, OUTPUT);
+  digitalWrite(2, LOW);
+  pinMode(4, OUTPUT);
+  digitalWrite(4, LOW);
+  pinMode(33, OUTPUT);
+  digitalWrite(33, LOW);
+  
+  // Connect to Wi-Fi
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+
+  // Print ESP Local IP Address
+  Serial.println(WiFi.localIP());
+
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/html", index_html, processor);
+  });
+
+  // Send a GET request to <ESP_IP>/update?output=<inputMessage1>&state=<inputMessage2>
+  server.on("/update", HTTP_GET, [] (AsyncWebServerRequest *request) {
+    String inputMessage1;
+    String inputMessage2;
+    // GET input1 value on <ESP_IP>/update?output=<inputMessage1>&state=<inputMessage2>
+    if (request->hasParam(PARAM_INPUT_1) && request->hasParam(PARAM_INPUT_2)) {
+      inputMessage1 = request->getParam(PARAM_INPUT_1)->value();
+      inputMessage2 = request->getParam(PARAM_INPUT_2)->value();
+      digitalWrite(inputMessage1.toInt(), inputMessage2.toInt());
+    }
+    else {
+      inputMessage1 = "No message sent";
+      inputMessage2 = "No message sent";
+    }
+    Serial.print("GPIO: ");
+    Serial.print(inputMessage1);
+    Serial.print(" - Set to: ");
+    Serial.println(inputMessage2);
+    request->send(200, "text/plain", "OK");
+  });
+
+  // Start server
+  server.begin();
 }
+
+
+
+
 void loop() {
-  myservo.write(45); // rotate the motor counterclockwise
+  myservo.write(100); // rotate counterclockwise, slow Speed
   delay(10000); // 10 secs
 
-  myservo.write(90); // stop the motor
+  myservo.write(90); // stop motor (send 1,5ms PWM Signal)
   delay(20000); // stay stopped 20 secs
 
-  //myservo.write(135); // rotate the motor clockwise
+
+/* Test 360° Servo Speedsettings
+  int angle;
+  // 0 to 180 degrees:
+  for (angle = 0; angle <= 180; angle += 1) {
+    myservo.write(angle);
+    Serial.println(angle);
+    delay(15);
+  }
+  // 180 to 0 degrees:
+  for (angle = 180; angle >= 0; angle -= 1) {
+    myservo.write(angle);
+    Serial.println(angle);
+    delay(30);
+  } */
 }
